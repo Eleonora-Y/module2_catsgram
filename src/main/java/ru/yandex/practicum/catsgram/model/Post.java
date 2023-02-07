66d4a5121339a71ddb0f21d39c73c7ee763f3ec9
Eleonora <106757028+Eleonora-Y@users.noreply.github.com>
2023-02-07 20:19:59 +0300

    feat: добавить обработку ошибок

--- src/main/java/ru/yandex/practicum/catsgram/model/Post.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/Post.java
@@ -2,43 +2,51 @@ package ru.yandex.practicum.catsgram.model;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.Instant;
-@Getter
-@Setter
+//@Getter
+        //@Setter
 public class Post {
-
+    private Integer id;
     private final String author; // автор
     private final Instant creationDate = Instant.now(); // дата создания
     private String description; // описание
     private String photoUrl; // url-адрес фотографии
 
 
-    public Post(String author, String description, String photoUrl) {
+    public Post(Integer id,String author, String description, String photoUrl) {
+        this.id = id;
         this.author = author;
         this.description = description;
       this.photoUrl = photoUrl;
     }
+    public Integer getId() {
+        return id;
+    }
 
-  //  public String getAuthor() {
-   //     return author;
-  //  }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-  //  public Instant getCreationDate() {
- //       return creationDate;
-  //  }
+    public String getAuthor() {
+        return author;
+    }
 
-//   public String getDescription() {
-//        return description;
-  //  }
+    public Instant getCreationDate() {
+        return creationDate;
+    }
 
- //   public void setDescription(String description) {
-  //      this.description = description;
- //   }
+    public String getDescription() {
+        return description;
+    }
 
- //  public String getPhotoUrl() {
- //       return photoUrl;
-  //  }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
- //   public void setPhotoUrl(String photoUrl) {
-  //      this.photoUrl = photoUrl;
-  //  }
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
 }
